d79500ac7d20b41ffd9bf4fffda4957d62f2340a
Stanislaus Madueke <stan.madueke@gmail.com>
2018-04-28 19:50:00 +0100

    Added payments

--- src/main/scala/com/lagosscala/hakathon/Routes.scala
+++ src/main/scala/com/lagosscala/hakathon/Routes.scala
@@ -7,6 +7,8 @@ import akka.http.scaladsl.server.Directives
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.Http
 import com.africastalking.sms._
+import com.africastalking.payment._
+import com.africastalking.core.utils.CurrencyCode
 import com.africastalking.sms.request.Message
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -31,18 +33,71 @@ trait Routes extends Directives {
             if (msg.text.filter(_ != '*').isEmpty) {
               complete("CON Did you forget your name?")
             } else {
-              val user = RegisteredUser(UUID.randomUUID.toString, msg.phoneNumber, msg.text)
-              users += (msg.phoneNumber -> user)
-              val message = Message(
-                text = s"Your conference registration code is ${user.id}",
-                recipients = List(msg.phoneNumber))
-              val response = SmsService.send(message).map { _ =>
-                "END Thanks for registering. Your should receive your ID by SMS."
-              } recover { case _: Throwable => "END Yawa don gas o." }
-              complete(response)
+              if (msg.text.equals("2")) {
+                if (users.contains(msg.phoneNumber)) {
+                  val card = PaymentCard(
+                    number = "5399832695266989",
+                    cvvNumber = 597,
+                    expiryMonth = 10,
+                    expiryYear = 2020,
+                    countryCode = "NG",
+                    authToken = "0000")
+
+                  val cardCheckout = CardCheckoutRequest(
+                    productName = "LSConf2018",
+                    currencyCode = CurrencyCode.NGN,
+                    amount = 500.00,
+                    cardDetails = card,
+                    narration = "Payment for Lagos Scala Conference")
+
+                  val response = PaymentService
+                    .cardCheckout(cardCheckout)
+                    .map { _ =>
+                      "END Thanks for your payment :)"
+                    } recover { case _: Throwable => "END Yawa don gas o." }
+                  complete(response)
+
+                } else {
+                  val message = """CON 
+                            Welcome to LSConf2018.
+                            Please enter your name:
+                            """
+                  complete(message)
+                }
+              } else {
+                val user = RegisteredUser(UUID.randomUUID.toString, msg.phoneNumber, msg.text)
+                users += (msg.phoneNumber -> user)
+                val message = Message(
+                  text = s"Your conference registration code is ${user.id}",
+                  recipients = List(msg.phoneNumber))
+                val response = SmsService.send(message).map { _ =>
+                  "END Thanks for registering. Your should receive your ID by SMS."
+                } recover { case _: Throwable => "END Yawa don gas o." }
+                complete(response)
+              }
             }
           }
         }
       }
     }
+
+  def cardPayment(): Unit = {
+    val card = PaymentCard(
+      number = "5399832695266989",
+      cvvNumber = 597,
+      expiryMonth = 10,
+      expiryYear = 2020,
+      countryCode = "NG",
+      authToken = "0000")
+
+    val cardCheckout = CardCheckoutRequest(
+      productName = "LSConf2018",
+      currencyCode = CurrencyCode.NGN,
+      amount = 500.00,
+      cardDetails = card,
+      narration = "Payment for Lagos Scala Conference")
+
+    PaymentService
+      .cardCheckout(cardCheckout)
+  }
 }
